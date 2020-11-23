b2eb757a5dca6a1c81a7db62d9429a1673817a6f
turtlelazy <ishraqmahid@gmail.com>
2020-11-23 16:32:05 -0500

    added math operators for real number

--- RealNumber.java
+++ RealNumber.java
@@ -13,5 +13,28 @@ public class RealNumber{
         return ""+getValue();
     }
 
-    
+    public RealNumber add(RealNumber other){
+        return new RealNumber(other.getValue() + getValue()); 
+    }
+
+    public RealNumber subtract(RealNumber other) {
+        return new RealNumber(getValue()-other.getValue());
+    }
+
+    public RealNumber multiply(RealNumber other) {
+        return new RealNumber(getValue()*other.getValue());
+    }
+
+    public RealNumber divide(RealNumber other){
+        return new RealNumber(getValue()/other.getValue());
+    }
+
+    public boolean equals(RealNumber other){
+        if(getValue() == 0.0 || other.getValue() == 0){
+            return getValue() == other.getValue();
+        }
+        else{
+            return (getValue() - other.getValue())* (getValue() - other.getValue()) < 0.000001;
+        }
+    }
 }
