22cfe5d057ec3aa8293261d4a71211b431a2a57a
turtlelazy <ishraqmahid@gmail.com>
2020-11-23 20:12:47 -0500

    constructor and getValue methods added for RationalNumber

--- RationalNumber.java
+++ RationalNumber.java
@@ -1,3 +1,23 @@
 public class RationalNumber extends RealNumber{
+    private int numerator, denominator;
+    
+    public RationalNumber(int nume, int deno){
+        super(0.0);
+        numerator = nume;
+        if(deno == 0){
+            numerator = 0;
+            denominator = 0;
+        }
+        else{
+            denominator = deno;
+        }
+    }
+
+    @Override
+    public double getValue(){
+        return numerator/denominator;
+
+    }
+
     
 }
