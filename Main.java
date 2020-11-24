871f5998926b501cee1c3cfdedd9b7f64c03c4b0
turtlelazy <ishraqmahid@gmail.com>
2020-11-23 20:47:29 -0500

    .equals method added and tested

--- Main.java
+++ Main.java
@@ -3,17 +3,21 @@ public class Main {
         RationalNumber x = new RationalNumber(0,1);
         System.out.print(x.getValue());
         System.out.println(" Should print 0.0");
-        System.out.println(x.getNum() + "/" + x.getDen());
+        System.out.println(x.getNumerator() + "/" + x.getDenominator());
 
         RationalNumber y = new RationalNumber(5, 0);
         System.out.print(y.getValue());
         System.out.println(" Should print 0.0");
-        System.out.println(y.getNum() + "/" + y.getDen());
+        System.out.println(y.getNumerator() + "/" + y.getDenominator());
 
         RationalNumber z = new RationalNumber(2, 3);
         System.out.print(z.getValue());
         System.out.println(" Should print 0.666...");
-        System.out.println(z.getNum() + "/" + z.getDen());
+        System.out.println(z.getNumerator() + "/" + z.getDenominator());
+
+        System.out.print(z.reciprocal().getValue());
+        System.out.println(" Should print 1.5");
+        System.out.println(z.reciprocal().getNumerator() + "/" + z.reciprocal().getDenominator());
 
     }
 }
--- RationalNumber.java
+++ RationalNumber.java
@@ -34,4 +34,5 @@ public class RationalNumber extends RealNumber{
     }
 
 
+
 }
