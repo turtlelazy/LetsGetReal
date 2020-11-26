622144802a414a39a2668ae58f7c17e4f94a7997
turtlelazy <ishraqmahid@gmail.com>
2020-11-25 23:50:49 -0500

    added equals method and abstract getValue method

--- Number.java
+++ Number.java
@@ -1,3 +1,15 @@
 public abstract class Number {
+    public abstract double getValue();
+
+    public boolean equals(Number other) {
+        if (getValue() == 0.0 || other.getValue() == 0.0) {
+            return getValue() == other.getValue();
+        } else {
+            return (getValue() - other.getValue()) * (getValue() - other.getValue()) < 0.000001;
+        }
+    }
+
     
+
+
 }
--- RealNumber.java
+++ RealNumber.java
@@ -29,12 +29,4 @@ public class RealNumber extends Number{
         return new RealNumber(getValue()/other.getValue());
     }
 
-    public boolean equals(RealNumber other){
-        if(getValue() == 0.0 || other.getValue() == 0.0){
-            return getValue() == other.getValue();
-        }
-        else{
-            return (getValue() - other.getValue())* (getValue() - other.getValue()) < 0.000001;
-        }
-    }
 }
